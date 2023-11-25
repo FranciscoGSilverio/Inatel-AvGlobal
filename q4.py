89348546755bb6eed76ac51246412261b11a5b5f
FranciscoGSilverio <franciscogsilverio@gmail.com>
2023-11-25 16:49:34 -0300

    add print to ex 4

--- q4.py
+++ q4.py
@@ -9,12 +9,10 @@ V2 = (c + 1) * 5
 
 R1, R2, R3 = 12, 4, 6
 
-eq1 = Eq(I1, I2 + I3)
-eq2 = Eq(V1, I1*R1 + I3*R3)
-eq3 = Eq(V2, I2*R2 - I3*R3)
+exp1 = Eq(I1, I2 + I3)
+exp2 = Eq(V1, I1*R1 + I3*R3)
+exp3 = Eq(V2, I2*R2 - I3*R3)
 
-solutions = solve((eq1, eq2, eq3), (I1, I2, I3))
+solutions = solve((exp1, exp2, exp3),(I1,I2, I3))
 
-print('I1: ', solutions[I1])
-print('I2: ', solutions[I2])
-print('I3: ', solutions[I3])
+print (solutions)
